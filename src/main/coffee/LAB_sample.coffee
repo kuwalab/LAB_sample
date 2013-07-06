9e14332af43abb1d1cec055904b6f2fc69093e2b
kuwalab <arata.shinichi@gmail.com>
2013-07-06 09:09:33 +0900

    初期化処理の分離
    ライブラリーのコードから、初期化部分を抜き出し

--- src/main/coffee/LAB_sample.coffee
+++ src/main/coffee/LAB_sample.coffee
@@ -3,24 +3,22 @@
     jquery: 'js/lib/jquery-2.0.3.min.js',
     jqueryui: 'js/lib/jquery-ui-1.10.3.custom.min.js'
 
-  loadList = (libList, callBack) ->
+  # ライブラリの名称の配列を渡す
+  # ライブラリをすべて読み込んだあと、callBackが実行される
+  # ライブラリはすべてwaitで読み込むため、遅い
+  # あくまでもプロトタイプ作成用
+  syncLoadLib = (libList, callBack) ->
     jsName = jsList[libList.shift()]
     $LAB.script(jsName).wait( ->
       if libList.length is 0
         do callBack
         return
       else
-        loadList(libList, callBack) 
+        syncLoadLib(libList, callBack)
     )
     return
 
-  loadJquery = () ->
-    loadList(['jquery', 'jqueryui'], () ->
-      $('#button').button()
-      return
-    )
-    return
-  
-  do loadJquery
+  global.syncLoadLib = syncLoadLib
+
   return
 )(this)
--- src/main/js/LAB_sample.js
+++ src/main/js/LAB_sample.js
@@ -1,24 +1,19 @@
 (function(global) {
-  var jsList, loadJquery, loadList;
+  var jsList, syncLoadLib;
   jsList = {
     jquery: 'js/lib/jquery-2.0.3.min.js',
     jqueryui: 'js/lib/jquery-ui-1.10.3.custom.min.js'
   };
-  loadList = function(libList, callBack) {
+  syncLoadLib = function(libList, callBack) {
     var jsName;
     jsName = jsList[libList.shift()];
     $LAB.script(jsName).wait(function() {
       if (libList.length === 0) {
         callBack();
       } else {
-        return loadList(libList, callBack);
+        return syncLoadLib(libList, callBack);
       }
     });
   };
-  loadJquery = function() {
-    loadList(['jquery', 'jqueryui'], function() {
-      $('#button').button();
-    });
-  };
-  loadJquery();
+  global.syncLoadLib = syncLoadLib;
 })(this);
